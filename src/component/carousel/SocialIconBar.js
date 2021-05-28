14b746ce7e481ff6ab586163d29dd38c78433ab2
Udit Kumar <uditkumarussa@gmail.com>
2021-05-29 01:29:39 +0530

    fixed warnings

--- src/component/carousel/SocialIconBar.js
+++ src/component/carousel/SocialIconBar.js
@@ -5,18 +5,15 @@ export function SocialIconBar() {
                 <button href="https://github.com/uditkumar01">
                     <i class="fab fa-github-alt"></i>
                 </button>
-                <button href="https://twitter.com">
+                <button href="https://twitter.com/uditkumar_01">
                     <i class="fab fa-twitter"></i>
                 </button>
-                <button href="https://linkedin.com">
+                <button href="https://linkedin.com/in/uditkumar01">
                     <i class="fab fa-linkedin-in"></i>
                 </button>
-                <button href="https://facebook.com">
-                    <i class="fab fa-facebook-f"></i>
-                </button>
             </span>
-            <button className="follow-text" href="#">
-                FOLLOW US
+            <button className="follow-text">
+                FOLLOW ME
             </button>
         </div>
     );
